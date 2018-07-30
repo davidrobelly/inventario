b6148df8bdea143147958917805354091839ebb9
David Robelly <david.robelly@gmail.com>
2018-07-30 10:25:03 -0500

    subiendo archivos

---
+++ admin/principal1.php
@@ -0,0 +1,68 @@
+
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 margin-bottom-10">
+                            <div class="dashboard-stat blue">
+                                <div class="visual">
+                                    <i class="fa fa-cart-arrow-down fa-icon-medium"></i>
+                                </div>
+                                <div class="details">
+                                    <div class="number"> Ingresos </div>
+                                    
+                                </div>
+                                <a class="more" href="?admin=entradas"> Ver Todos
+                                    <i class="m-icon-swapright m-icon-white"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                            <div class="dashboard-stat red">
+                                <div class="visual">
+                                    <i class="fa fa-chevron-circle-up fa-icon-medium"></i>
+                                </div>
+                                <div class="details">
+                                    <div class="number"> Salidas </div>
+                                    
+                                </div>
+                                <a class="more" href="?admin=salida"> Ver Todos
+                                    <i class="m-icon-swapright m-icon-white"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                            <div class="dashboard-stat green">
+                                <div class="visual">
+                                    <i class="fa fa-shopping-cart fa-icon-medium"></i>
+                                </div>
+                                <div class="details">
+                                    <div class="number"> Productos </div>
+                                   
+                                </div>
+                                <a class="more" href="?admin=productos"> Ver Todos
+                                    <i class="m-icon-swapright m-icon-white"></i>
+                                </a>
+                            </div>
+                        </div> -->
+                    </div>
+
+                    <div class="row">
+                       
+                        <h1><center><strong>Bienvenido al Sistema de Inventario y Control de Ingresos/Salidas de productos</strong></center></h1>
+                        <br>
+                        <h1><center><strong>Fainca Group C.A.</strong></center></h1>
+                            
+                        
+                    </div>
+                    
+                </div>
+                <!-- END CONTENT BODY -->
+            </div>
+           
+      <!--  grafica 1-->
+    
+    <script type="text/javascript" src="assets/graficas/canvasjs.min.js"></script>
+    <script type="text/javascript" src="js/graficas.js"></script>
+         
+
+
+    
+ 
